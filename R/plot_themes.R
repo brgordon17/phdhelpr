10834cce322a377d8f343543dbebd451b9ed32ef
Benjamin R. Gordon <brgordon17@gmail.com>
2019-07-25 13:00:12 +1000

    Initial commit

---
+++ R/plot_themes.R
@@ -0,0 +1,12 @@
+#' ggplot2 Theme Template
+#'
+#' A standard theme template for ggplot2 used in BRGs thesis
+#'
+#' @author Benjamin R. Gordon
+#'
+#' @seealso \href{http://serialmentor.com/dataviz/color-pitfalls.html#not-designing-for-color-vision-deficiency}{serialmentor.com}
+#' and \href{http://jfly.iam.u-tokyo.ac.jp/color/}{Okabe and Ito (2008)}
+#'
+#' @export
+#'
+theme_brg <- theme(axis.title = element_blank())
