12109b673c3cb9afe2b3f846eb6b439ce719a807
Benjamin R. Gordon <brgordon17@gmail.com>
2019-07-25 12:46:36 +1000

    Initial commit

---
+++ R/colour_palettes.R
@@ -0,0 +1,54 @@
+#' Qualitative Colour Palette
+#'
+#' Qualitative, colour blind safe colour palette developed by Okabe and Ito
+#'
+#' @author Benjamin R. Gordon
+#'
+#' @seealso \href{http://serialmentor.com/dataviz/color-pitfalls.html#not-designing-for-color-vision-deficiency}{serialmentor.com}
+#' and \href{http://jfly.iam.u-tokyo.ac.jp/color/}{Okabe and Ito (2008)}
+#'
+#' @export
+#'
+qual_colours <- c("#E69F00",
+                  "#56B4E9",
+                  "#009E73",
+                  "#F0E442",
+                  "#0072B2",
+                  "#D55E00",
+                  "#CC79A7",
+                  "#999999")
+
+#' Sarah and Duck Colours
+#'
+#' Colour palette derived from the TV show Sarah and Duck.
+#'
+#' @seealso
+#' \href{https://www.sarahandduck.com}{sarahandduck.com}
+#'
+#' @author Benjamin R. Gordon
+#'
+#' @export
+#'
+sd_colours <- c("#73BE97",
+                "#595653",
+                "#3CB54C",
+                "#E9898D",
+                "#D2CA65")
+
+#' Yellow Orange Red Sequential colours
+#'
+#' Five sequential colours from the YlOrRd palette at colorbrewer.org.
+#'
+#' @seealso
+#' \href{http://colorbrewer2.org/#type=sequential&scheme=YlOrRd&n=5}{colorbrewer.org}
+#'
+#' @author Benjamin R. Gordon
+#'
+#' @export
+#'
+warm_colours  <- c("#ffffb2",
+                   "#fecc5c",
+                   "#fd8d3c",
+                   "#f03b20",
+                   "#bd0026")
+
